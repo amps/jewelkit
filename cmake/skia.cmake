e20835d720eea6394c3d9953cf94ef2d04766fe4
Conner Bryan <conner.bryan@josflare.com>
2014-01-31 11:27:23 -0500

    fix skia deps

---
+++ cmake/skia.cmake
@@ -0,0 +1,13 @@
+SET(Skia_Base ${PROJECT_SOURCE_DIR}/deps/skia/out/Release/libskia_)
+SET(Skia_LIBS ${Skia_Base}core.a ${Skia_Base}images.a ${Skia_Base}skgpu.a ${Skia_Base}ports.a ${Skia_Base}opts.a ${Skia_Base}opts_ssse3.a)
+
+add_custom_command(
+OUTPUT ${Skia_LIBS}
+COMMAND make skia_lib BUILDTYPE=Release
+WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/deps/skia
+)
+
+add_custom_target(
+skia_lib
+DEPENDS  ${Skia_LIBS}
+)
---
+++ lib/toolkit.cpp
@@ -0,0 +1,2 @@
+#include <core/SkCanvas.h>
+
